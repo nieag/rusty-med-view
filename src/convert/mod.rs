faa8653d9ca79d5fdc6abf472487a1f2596e2725
Niels Wessén <niels.agerskov@gmail.com>
2026-02-14 17:18:23 +0100

    Phase C: Surface Nets isosurface extractor

--- src/convert/mod.rs
+++ src/convert/mod.rs
@@ -8,9 +8,11 @@ pub mod contour_to_sdf;
 pub mod contour_to_tsdf_chunks;
 pub mod labelmap_to_contours;
 pub mod marching_cubes;
+pub mod surface_nets;
 
 pub use chunk_grid::*;
 pub use contour_to_sdf::*;
 pub use contour_to_tsdf_chunks::*;
 pub use labelmap_to_contours::*;
 pub use marching_cubes::*;
+pub use surface_nets::*;
